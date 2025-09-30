6af0621a71da86e34f07493413151628e1285ab8
CupheadUltra <129087076+CupheadUltra@users.noreply.github.com>
2025-09-30 20:23:04 +0300

    Updates

--- src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Button({ onClick }) {
-  return <button onClick={onClick}>Натисни мене</button>;
-}
-
-export default Button;
--- src/components/Greeting.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Greeting({ name }) {
-  return <h1>Привіт, {name}!</h1>;
-}
-
-export default Greeting;
